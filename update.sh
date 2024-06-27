95dff1abeedf814f4c2e66eb83c25ebe06bfcf77
myvpn1 <andyyuda630@gmail.com>
2024-06-27 17:30:15 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -49,7 +49,7 @@ fun_bar() {
 # Fungsi untuk mengunduh, mengekstrak, dan memindahkan file
 res1() {
     wget --no-check-certificate https://konohagakure.klmpk.me:81/limit/menu.zip
-    wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/myvpn1/vip/limit/main/epro/epro" ; chmod +x /usr/bin/enc
+    wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/myvpn1/vip/main/limit/epro/epro" ; chmod +x /usr/bin/enc
     7z e -paskyandy123 x menu.zip
     unzip menu.zip
     chmod +x menu/*
